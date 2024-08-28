1fdacf1fc51a848269db4102327094d2d89d64c1
Regal-Cool <69114064+regalcll@users.noreply.github.com>
2024-08-28 18:28:02 +0800

    Update 手动选择.js

--- 手动选择.js
+++ 手动选择.js
@@ -1,3 +1,9 @@
+// 参考 Verge Rev 示例 Script 配置
+//
+// Clash Verge Rev (Version ≥ 17.2) & Mihomo-Party (Version ≥ 0.5.8)
+//
+// 最后更新时间: 2024-8-24 22:00
+
 // 规则集通用配置
 const ruleProviderCommon = {
   "type": "http",
@@ -8,7 +14,7 @@ const ruleProviderCommon = {
 // 策略组通用配置
 const groupBaseOption = {
   "interval": 300,
-  "url": "http://www.gstatic.com/generate_204",
+  "url": "http://connectivitycheck.gstatic.com/generate_204",
   "max-failed-times": 3,
 };
 
@@ -22,98 +28,28 @@ function main(config) {
   }
 
   // 覆盖通用配置
-  config["mixed-port"] = "7893";
+  config["mixed-port"] = "7890";
   config["tcp-concurrent"] = true;
   config["allow-lan"] = true;
   config["ipv6"] = false;
-  config["mode"] = "rule";
   config["log-level"] = "info";
   config["find-process-mode"] = "strict";
   config["global-client-fingerprint"] = "chrome";
+  config["external-controller"] = "127.0.0.1:9090";
+  config["external-ui"] = "ui";
+  config["external-ui-url"] = "https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip";
 
   // 覆盖 dns 配置
   config["dns"] = {
     "enable": true,
     "listen": "0.0.0.0:1053",
-    "ipv6": true,
+    "ipv6": false,
     "enhanced-mode": "fake-ip",
     "fake-ip-range": "198.18.0.1/16",
-    "fake-ip-filter":[
-      "*.lan",
-      "*.direct",
-      "cable.auth.com",
-      "*.msftconnecttest.com",
-      "*.msftncsi.com",
-      "network-test.debian.org",
-      "detectportal.firefox.com",
-      "resolver1.opendns.com",
-      "*.srv.nintendo.net",
-      "*.stun.playstation.net",
-      "xbox.*.microsoft.com",
-      "*.xboxlive.com",
-      "stun.*",
-      "global.turn.twilio.com",
-      "global.stun.twilio.com",
-      "app.yinxiang.com",
-      "injections.adguard.org",
-      "local.adguard.org",
-      "cable.auth.com",
-      "localhost.*.qq.com",
-      "localhost.*.weixin.qq.com",
-      "*.logon.battlenet.com.cn",
-      "*.logon.battle.net",
-      "*.blzstatic.cn",
-      "music.163.com",
-      "*.music.163.com",
-      "*.126.net",
-      "musicapi.taihe.com",
-      "music.taihe.com",
-      "songsearch.kugou.com",
-      "trackercdn.kugou.com",
-      "*.kuwo.cn",
-      "api-jooxtt.sanook.com",
-      "api.joox.com",
-      "joox.com",
-      "y.qq.com",
-      "*.y.qq.com",
-      "streamoc.music.tc.qq.com",
-      "mobileoc.music.tc.qq.com",
-      "isure.stream.qqmusic.qq.com",
-      "dl.stream.qqmusic.qq.com",
-      "aqqmusic.tc.qq.com",
-      "amobile.music.tc.qq.com",
-      "*.xiami.com",
-      "*.music.migu.cn",
-      "music.migu.cn",
-      "proxy.golang.org",
-      "*.mcdn.bilivideo.cn",
-      "*.cmpassport.com",
-      "id6.me",
-      "open.e.189.cn",
-      "mdn.open.wo.cn",
-      "opencloud.wostore.cn",
-      "auth.wosms.cn",
-      "*.jegotrip.com.cn",
-      "*.icitymobile.mobi",
-      "*.pingan.com.cn",
-      "*.cmbchina.com",
-      "*.10099.com.cn",
-      "pool.ntp.org",
-      "*.pool.ntp.org",
-      "ntp.*.com",
-      "time.*.com",
-      "ntp?.*.com",
-      "time?.*.com",
-      "time.*.gov",
-      "time.*.edu.cn",
-      "*.ntp.org.cn",
-      "PDC._msDCS.*.*",
-      "DC._msDCS.*.*",
-      "GC._msDCS.*.*"
-    ],
-    "default-nameserver": ["223.5.5.5", "119.29.29.29"],
+    "fake-ip-filter": ["*", "+.lan", "+.local", "+.direct", "+.msftconnecttest.com", "+.msftncsi.com"],
+    "default-nameserver": ["223.5.5.5", "119.29.29.29", "system"],
     "nameserver": ["223.5.5.5", "119.29.29.29"],
-    "nameserver-policy":{
+    "nameserver-policy": {
       "geosite:cn": "system",
       "geosite:gfw,geolocation-!cn": ["quic://223.5.5.5", "quic://223.6.6.6", "https://1.12.12.12/dns-query", "https://120.53.53.53/dns-query"]
     }
@@ -124,7 +60,8 @@ function main(config) {
   config["geox-url"] = {
     "geoip": "https://mirror.ghproxy.com/https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat",
     "geosite": "https://mirror.ghproxy.com/https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat",
-    "mmdb": "https://mirror.ghproxy.com/https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb"
+    "mmdb": "https://mirror.ghproxy.com/https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb",
+    "asn": "https://mirror.ghproxy.com/https://github.com/xishang0128/geoip/releases/download/latest/GeoLite2-ASN.mmdb"
   };
 
   // 覆盖 sniffer 配置
@@ -149,9 +86,7 @@ function main(config) {
   config["tun"] = {
     "enable": true,
     "stack": "mixed",
-    "dns-hijack": ["any:53"],
-    "auto-route": true,
-    "auto-detect-interface": true  
+    "dns-hijack": ["any:53"]
   };
 
   // 覆盖策略组
@@ -165,6 +100,13 @@ function main(config) {
     },
     {
       ...groupBaseOption,
+      "name": "Media",
+      "type": "select",
+      "include-all": true,
+      "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/YouTube.png"
+    },
+    {
+      ...groupBaseOption,
       "name": "Chat",
       "type": "select",
       "include-all": true,
@@ -172,45 +114,75 @@ function main(config) {
     },
     {
       ...groupBaseOption,
-      "name": "Media",
-      "type": "select",
-      "include-all": true,
-      "icon": "https://raw.githubusercontent.com/Orz-3/face/master/YouTube.png"
-    },
-    {
-      ...groupBaseOption,
       "name": "Emby",
       "type": "select",
       "include-all": true,
       "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Emby.png"
+    },
+    {
+      ...groupBaseOption,
+      "name": "AD",
+      "type": "select",
+      "proxies": ["REJECT", "DIRECT"],
+      "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Advertising.png"
     }
   ];
 
   // 覆盖规则集
   config["rule-providers"] = {
+    "AD": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Reject.list",
+      "path": "./rule-providers/AD.list"
+    },
     "Apple": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Apple/Apple.list",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Apple.list",
       "path": "./rule-providers/Apple.list"
+    },
+    "Google": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Google.list",
+      "path": "./rule-providers/Google.list"
     },
     "YouTube": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.list",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/YouTube.list",
       "path": "./rule-providers/YouTube.list"
     },
     "Telegram": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.list",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Telegram.list",
       "path": "./rule-providers/Telegram.list"
     },
-    "Discord": {
+    "Twitter": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Discord/Discord.list",
-      "path": "./rule-providers/Discord.list"
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Twitter.list",
+      "path": "./rule-providers/Twitter.list"
+    },
+    "Steam": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Steam.list",
+      "path": "./rule-providers/Steam.list"
+    },
+    "Epic": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Epic.list",
+      "path": "./rule-providers/Epic.list"
+    },
+    "AI": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/AI.list",
+      "path": "./rule-providers/AI.list"
     },
     "Emby": {
       ...ruleProviderCommon,
@@ -218,31 +190,103 @@ function main(config) {
       "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Emby.list",
       "path": "./rule-providers/Emby.list"
     },
+    "Spotify": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Spotify.list",
+      "path": "./rule-providers/Spotify.list"
+    },
+    "Bahamut": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Bahamut.list",
+      "path": "./rule-providers/Bahamut.list"
+    },
+    "Netflix": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Netflix.list",
+      "path": "./rule-providers/Netflix.list"
+    },
     "Disney": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Disney/Disney.list",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Disney.list",
       "path": "./rule-providers/Disney.list"
     },
-    "Global": {
+    "PrimeVideo": {
       ...ruleProviderCommon,
       "behavior": "classical",
-      "url": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Global/Global.list",
-      "path": "./rule-providers/Global.list"
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/PrimeVideo.list",
+      "path": "./rule-providers/PrimeVideo.list"
+    },
+    "HBO": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/HBO.list",
+      "path": "./rule-providers/HBO.list"
+    },
+    "OneDrive": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/OneDrive.list",
+      "path": "./rule-providers/OneDrive.list"
+    },
+    "Github": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Github.list",
+      "path": "./rule-providers/Github.list"
+    },
+    "Microsoft": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Microsoft.list",
+      "path": "./rule-providers/Microsoft.list"
+    },
+    "Lan": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/Lan.list",
+      "path": "./rule-providers/Lan.list"
+    },
+    "ProxyGFW": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/ProxyGFW.list",
+      "path": "./rule-providers/ProxyGFW.list"
+    },
+    "China": {
+      ...ruleProviderCommon,
+      "behavior": "classical",
+      "url": "https://github.com/Repcz/Tool/raw/X/Clash/Rules/ChinaDomain.list",
+      "path": "./rule-providers/China.list"
     }
   };
 
   // 覆盖规则
   config["rules"] = [
+    "RULE-SET,AD,AD",
+    "RULE-SET,AI,Proxy",
     "RULE-SET,Apple,DIRECT",
-    "RULE-SET,Telegram,Chat",
-    "RULE-SET,Discord,Chat",
     "RULE-SET,YouTube,Media",
-    "RULE-SET,Disney,Media",
+    "RULE-SET,Google,Proxy",
+    "RULE-SET,Telegram,Chat",
+    "RULE-SET,Twitter,Proxy",
+    "RULE-SET,Steam,DIRECT",
+    "RULE-SET,Epic,DIRECT",
     "RULE-SET,Emby,Emby",
-    "RULE-SET,Global,Proxy",
+    "RULE-SET,Spotify,Media",
+    "RULE-SET,Bahamut,Media",
+    "RULE-SET,Netflix,Media",
+    "RULE-SET,Disney,Media",
+    "RULE-SET,PrimeVideo,Media",
+    "RULE-SET,HBO,Media",
+    "GEOSITE,onedrive,DIRECT",
+    "GEOSITE,github,DIRECT",
+    "GEOSITE,microsoft,DIRECT",
     "GEOSITE,gfw,Proxy",
-    "GEOSITE,cn,DIRECT",
+    "RULE-SET,China,DIRECT",
     "GEOIP,lan,DIRECT",
     "GEOIP,CN,DIRECT",
     "MATCH,Proxy"
